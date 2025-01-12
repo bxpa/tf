1cca1ede9250eb25526a91a5f0a9fba3420e25bc
bxpa <martinfidjeland@gmail.com>
2025-01-12 12:51:06 +0100

    implemented tf-class

--- tf/tf.py
+++ tf/tf.py
@@ -2,41 +2,49 @@ import functools
 import timeit
 import typing
 
-available_formats = ('ms', 'sec', 'min', 'hr')
 
+class tf:
+    available_formats: tuple = ("ms", "sec", "min", "hr")
 
-def tf(func: typing.Optional[typing.Callable] = None, *, precision: int = 3, time_format: str = "ms") -> typing.Callable:
-    if precision < 0:
-        raise ValueError('Precision must be greater than 0')
-    if time_format not in available_formats:
-        print(time_format)
-        raise ValueError(f"""Time format must be one of these arguments: {
-                         ", ".join(available_formats)}""")
-    if func is None:
-        return functools.partial(tf, precision=precision, time_format=time_format)
+    def __init__(self, precision: int = 3, time_format: str = "ms"):
+        if precision < 0:
+            raise ValueError("Precision must be greater than 0")
+        if time_format not in self.available_formats:
+            raise ValueError(
+                f"Time format must be one of these arguments: {', '.join(self.available_formats)}"
+            )
+        self.precision = precision
+        self.time_format = time_format
 
-    @functools.wraps(func)
-    def decorated(*args: typing.Any, **kwargs: typing.Any) -> typing.Any:
-        start_time = timeit.default_timer()
-        result = func(*args, **kwargs)
-        end_time = timeit.default_timer()
-        elapsed_time = end_time - start_time
+    def __call__(
+        self, func: typing.Optional[typing.Callable] = None
+    ) -> typing.Callable:
+        if func is None:
+            return functools.partial(self.__call__)
 
-        match time_format.lower():
-            case "ms":
-                elapsed_time *= 1000
-                unit = "ms"
-            case "min":
-                elapsed_time /= 60
-                unit = "min"
-            case "hr":
-                elapsed_time /= 3600
-                unit = "hr"
-            case _:
-                unit = "sec"
+        @functools.wraps(func)
+        def decorated(*args: typing.Any, **kwargs: typing.Any) -> typing.Any:
+            start_time = timeit.default_timer()
+            result = func(*args, **kwargs)
+            end_time = timeit.default_timer()
+            elapsed_time = end_time - start_time
 
-        print(f"""Function '{func.__name__}' executed in {
-              elapsed_time:.{precision}f} {unit}""")
-        return result
+            match self.time_format.lower():
+                case "ms":
+                    elapsed_time *= 1000
+                    unit = "ms"
+                case "min":
+                    elapsed_time /= 60
+                    unit = "min"
+                case "hr":
+                    elapsed_time /= 3600
+                    unit = "hr"
+                case _:
+                    unit = "sec"
 
-    return decorated
+            print(
+                f"Function '{func.__name__}' executed in {elapsed_time:.{self.precision}f} {unit}"
+            )
+            return result
+
+        return decorated
